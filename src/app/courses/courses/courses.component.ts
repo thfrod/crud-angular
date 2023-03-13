8e285718c9cf215f54b97b3ebeaab156d72d8a1a
Thiago Ferreira Rodrigues <thiago.ferreirarod22@gmail.com>
2023-03-13 13:18:32 -0300

    feat: adding courses module

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,13 @@
+import { Course } from './../model/course';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
+  courses: Course[] = [];
 
+  displayedColumns = ['id', 'name', 'category'];
 }
